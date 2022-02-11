4e9be50d50822f15c33cba31b7ad2f521773e2c5
Turo <turo>
2022-02-11 13:53:35 +0200

    Lsiätty many-to-many suhde taulujen välille

--- sakila/models.py
+++ sakila/models.py
@@ -9,9 +9,17 @@ class Film(models.Model):
     length = models.IntegerField()
     release_date = models.DateField()
 
+    def __str__(self):
+        return self.title
+
 class Actor(models.Model):
 
     actor_id = models.BigAutoField(primary_key=True)
     first_name = models.CharField(max_length=60)
     last_name = models.CharField(max_length=60)
     last_modified = models.DateTimeField(auto_now=True)
+
+    films = models.ManyToManyField(Film)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
